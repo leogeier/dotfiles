8c965312b6d0814dd3b9bc00941f116d854b7fca
leogeier <leonard.geier@hotmail.com>
2020-01-25 13:23:36 +0100

    Improve init script

--- .init-dotfiles.sh
+++ .init-dotfiles.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-git clone --bare https://github.com/leogeier/dotfiles.git $HOME/.cfg
+git clone --bare https://github.com/leogeier/dotfiles.git $HOME/.cfg || exit $?
 function dotconfig {
   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 dotconfig checkout &> /dev/null
 if [ $? = 0 ]; then
-  echo "Checked out config.";
+  echo "Checked out config."
 else
-  echo "Backing up pre-existing dot files.";
+  echo "Backing up pre-existing dot files:"
   mkdir -p $HOME/.config-backup
-  dotconfig checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  dotconfig checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | tee /dev/stderr | xargs -I{} mv {} .config-backup/{}
+  dotconfig checkout
+  echo "Checked out config."
 fi;
-dotconfig checkout
 dotconfig config status.showUntrackedFiles no
+echo "All done, feel free to delete this script now."
