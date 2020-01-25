804aff92e3ed567631a069e21305ed23370f0e3a
leogeier <leonard.geier@hotmail.com>
2020-01-25 13:08:08 +0100

    Fix install script once more once more

--- .init-dotfiles.sh
+++ .init-dotfiles.sh
@@ -4,13 +4,13 @@ git clone --bare https://github.com/leogeier/dotfiles.git $HOME/.cfg
 function dotconfig {
   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-dotconfig checkout
+dotconfig checkout &> /dev/null
 if [ $? = 0 ]; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
   mkdir -p $HOME/.config-backup
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  dotconfig checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 dotconfig checkout
 dotconfig config status.showUntrackedFiles no
