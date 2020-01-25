8291744e47b25f9a1634bfa46a79b33b6e0bafeb
leogeier <leonard.geier@hotmail.com>
2020-01-25 13:03:15 +0100

    Fix install script once more

--- .init-dotfiles.sh
+++ .init-dotfiles.sh
@@ -4,12 +4,12 @@ git clone --bare https://github.com/leogeier/dotfiles.git $HOME/.cfg
 function dotconfig {
   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p $HOME/.config-backup
 dotconfig checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
+  mkdir -p $HOME/.config-backup
   config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 dotconfig checkout
